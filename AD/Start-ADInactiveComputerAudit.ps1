8f710ad835de5f72d2d9e9164f22257cd1b06d2b
Gabe Feindel <gabefeindel@gmail.com>
2020-03-10 07:35:26 -0400

    - Add inactive user/computer report scripts

---
+++ AD/Start-ADInactiveComputerAudit.ps1
@@ -0,0 +1,139 @@
+﻿<#
+.SYNOPSIS
+Identifies inactive computer accounts and e-mails the appropriate site support person.
+
+.DESCRIPTION
+Queries Active Directory for inactive computer accounts and sends e-mail audit reports
+to the site support personnel.
+#>
+
+# This list must be updated whenever a new site is added to AD.
+$SiteList = @{
+    #<SiteCode> = "<email1>[,<email2>]"
+}
+
+function Get-InactiveComputers {
+<#
+.DESCRIPTION
+Retrieves inactive computers for the given site.
+.PARAMETER ADSite
+The name of the OU under OU=Computers,...
+
+.PARAMETER InactiveDays
+The number of days since the last sign in after which a user is
+considered inactive.
+#>
+    param(
+        [parameter(Mandatory=$true)]
+        [string]$ADSite,
+        [int]$InactiveDays = 90,
+        [string]$CC,
+        [string]$From,
+        [string]$SmtpServer
+    )
+
+    $ouPath = ""
+    $cutoffDate = (Get-Date).AddDays(-$InactiveDays)
+    $computers = Get-ADComputer -Filter * -SearchBase $ouPath -Properties LastLogonDate |
+                    Where-Object {$_.LastLogonDate -lt $cutoffDate}
+    
+    $computers
+}
+
+function Format-InactiveComputerReport {
+<#
+.DESCRIPTION
+    Converts an inactive computers list to nicely formatted HTML.
+.PARAMETER data
+    The data to be formatted in the report.
+.PARAMETER Fields
+    The list of field names to include in the tabular listing.
+.PARAMETER Overview
+    The summary statement to include before the table.
+.PARAMETER Title
+    The title of the report.
+#>
+    param(
+        [parameter(Mandatory=$true)]
+        [psobject[]]$data,
+        [parameter(Mandatory=$true)]
+        [string[]]$Fields,
+        [parameter(Mandatory=$true)]
+        [string]$Overview,
+        [parameter(Mandatory=$true)]
+        [string]$Title
+    )
+
+    $head = @"
+<style type='text/css'>
+BODY {
+    font-family: Arial, sans serif;
+    font-size: 11pt;
+}
+TABLE {
+    border-width: 1px;
+    border-style: solid;
+    border-color: #AAA;
+    border-collapse: collapse;
+}
+TH {
+    background-color: #009;
+    color: white;
+}
+TH, TD {
+    padding: 0px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #AAA;
+}
+
+</style>
+"@
+
+    $body = "<h2>$Title</h2>"
+    $body += "<p>$Overview</p>"
+    write-host ($data | Select-Object $Fields)
+    $html = $data | Select-Object $Fields | ConvertTo-Html -Head $head -Body $body
+    $html
+}
+
+$date = (Get-Date).ToShortDateString()
+
+foreach($site in $SiteList.Keys) {
+
+    $title = "$site Inactive Computers Report $date"
+    $recipient = $SiteList[$site]
+
+    Write-Host "Searching for inactive computers in $site."
+    $computers = Get-InactiveComputers -ADSite $site | Sort-Object -Property $LastLogonDate
+    $computerCount = ($computers | Measure-Object).Count
+    
+    if($computerCount -eq 0) {
+        write-Host "No inactive computers in $Site."
+        continue
+    }
+
+    $parms = @{
+        Fields = @('Name','LastLogonDate','DistinguishedName')
+        Title = $title
+        Overview = "$computerCount $site computers have not connected for 90 days or more. Please disable them or delete them."
+        Data = $computers
+    }
+    
+    $report = Format-InactiveComputerReport @parms
+    #$report | Out-File -FilePath "C:\FM\Reports\Inactive computers $Site.htm"
+
+    Write-Host "$computerCount inactive computers found in $site."
+    Write-Host "Sending report to $recipient"
+    $parms = @{
+        Body = [string]::join("",$report)
+        BodyAsHtml = $true
+        To = $recipient
+        CC = $CC
+        From = $From
+        Subject = $title
+        SmtpServer = $SmtpServer
+    }
+    Send-MailMessage @parms
+
+}
---
+++ AD/Start-ADInactiveUserAudit.ps1
@@ -0,0 +1,141 @@
+﻿<#
+.SYNOPSIS
+Identifies inactive user accounts and e-mails the appropriate site support person.
+
+.DESCRIPTION
+Queries Active Directory for inactive user accounts and sends e-mail audit reports
+to the site support personnel.
+#>
+[CmdletBinding()]
+param()
+
+$SiteList = @{
+    #<SiteCode> = "<email1>[,<email2>]"
+}
+
+function Get-InactiveUsers {
+    <#
+    .DESCRIPTION
+    Retrieves inactive users for the given site.
+    .PARAMETER ADSite
+    The name of the OU under OU=Users,OU=FMEDICAL,DC=FMEDICAL,DC=NET
+
+    .PARAMETER InactiveDays
+    The number of days since the last sign in after which a user is
+    considered inactive.
+    #>
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true)]
+        [string]$ADSite,
+        [int]$InactiveDays = 90,
+        [string]$CC,
+        [string]$From,
+        [string]$SmtpServer
+    )
+
+    $ouPath = ""
+    $timespan = New-TimeSpan -Days $InactiveDays
+    $users = Search-ADAccount -AccountInactive -TimeSpan $timespan -SearchBase $ouPath
+    
+    $users
+}
+
+function Format-InactiveUserReport {
+    <#
+    .DESCRIPTION
+        Converts an inactive users list to nicely formatted HTML.
+    .PARAMETER data
+        The data to be formatted in the report.
+    .PARAMETER Fields
+        The list of field names to include in the tabular listing.
+    .PARAMETER Overview
+        The summary statement to include before the table.
+    .PARAMETER Title
+        The title of the report.
+    #>
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true)]
+        [psobject[]]$data,
+        [parameter(Mandatory=$true)]
+        [string[]]$Fields,
+        [parameter(Mandatory=$true)]
+        [string]$Overview,
+        [parameter(Mandatory=$true)]
+        [string]$Title
+    )
+
+    $head = @"
+<style type='text/css'>
+BODY {
+    font-family: Arial, sans serif;
+    font-size: 11pt;
+}
+TABLE {
+    border-width: 1px;
+    border-style: solid;
+    border-color: #AAA;
+    border-collapse: collapse;
+}
+TH {
+    background-color: #009;
+    color: white;
+}
+TH, TD {
+    padding: 0px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #AAA;
+}
+
+</style>
+"@
+
+    $body = "<h2>$Title</h2>"
+    $body += "<p>$Overview</p>"
+    Write-Verbose ($data | Select-Objet $Fields)
+    $html = $data | Select-Object $Fields | ConvertTo-Html -Head $head -Body $body
+    $html
+}
+
+$date = (Get-Date).ToShortDateString()
+
+foreach($site in $SiteList.Keys) {
+
+    $title = "$site Inactive Users Report $date"
+    $recipient = $SiteList[$site]
+
+    Write-Verbose "Searching for inactive users in $site."
+    $users = Get-InactiveUsers -ADSite $site | Sort-Object -Property $LastLogonDate
+    $usercount = ($users | Measure-Object).Count
+    
+    if($usercount -eq 0) {
+        Write-Verbose "No inactive users in $Site."
+        continue
+    }
+
+    $parms = @{
+        Fields = @('Name','LastLogonDate','DistinguishedName')
+        Title = $title
+        Overview = "$usercount $site users have not signed in for 90 days or more. Please disable them or delete them."
+        Data = $users
+    }
+    
+    $report = Format-InactiveUserReport @parms
+    #$report | Out-File -FilePath "C:\FM\Reports\Inactive Users $Site.htm"
+
+    Write-Verbose "$usercount inactive users found in $site."
+    Write-Verbose "Sending report to $recipient"
+    $parms = @{
+        Body = [string]::join("",$report)
+        BodyAsHtml = $true
+        To = $recipient
+        CC = $CC
+        From = $From
+        Subject = $title
+        SmtpServer = $SmtpServer
+    }
+    Send-MailMessage @parms
+
+}
