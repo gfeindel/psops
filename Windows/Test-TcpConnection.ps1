46382deda585159dab56a0d85f31ac5e398ef17e
Gabe Feindel <gabefeindel@gmail.com>
2020-04-12 09:32:10 -0400

    Test-TcpConnection.ps1: Add CTRL-C handling, session stats.

--- Windows/Test-TcpConnection.ps1
+++ Windows/Test-TcpConnection.ps1
@@ -16,15 +16,23 @@ function Write-Log {
     "$strTime $msg" | Out-File -FilePath $LogFile -Append -Encoding Utf8
 }
 
+[Console]::TreatControlCAsInput = $true
+Start-Sleep -Seconds 1
+$Host.UI.RawUI.FlushInputBuffer()
 
+[int]$total = 0
+[int]$success = 0
+[datetime]$startTime = Get-Date
 
 while($true) {
+    $total++
     try {
         $tcp = New-Object System.Net.Sockets.TcpClient
         $tcp.Connect($Destination,$Port)
         if($tcp.Connected) {
             Write-Log "Successfully connected to ${Destination}:${Port}"
             $tcp.Close()
+            $success++
         } else {
             Write-Log "Not connected."
         }
@@ -33,7 +41,15 @@ while($true) {
     } finally {
         $tcp.Dispose()
     }
+    # Check for CTRL+C
+    if($Host.UI.RawUI.KeyAvailable -and ($Key = $Host.UI.RawUI.ReadKey("AllowCtrlC,NoEcho,IncludeKeyUp"))) {
+        if([Int]$Key.Character -eq 3) {
+            break;
+        }
+        $Host.UI.RawUI.FlushInputBuffer()
+    }
     Start-Sleep -Milliseconds $Timeout
 }
-
-
+$successRate = [int]($success/$total*100)
+$duration = [datetime]::Now - $startTime
+Write-Host "$successRate% success, $(100-$successRate)% failed. Duration: $duration"
